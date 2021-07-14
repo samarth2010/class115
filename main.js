d9720ad978b1af862b4e76f7f70953f3b0c97cb2
samarth2010 <76905546+samarth2010@users.noreply.github.com>
2021-07-14 20:59:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function preload(){}
+function setup(){
+canvas=createCanvas(350,300);
+canvas.center();
+video = createCapture(VIDEO);
+video.size(350,300);
+video.hide();
+poseNet= ml5.poseNet(video,modelloaded);
+poseNet.on('pose',gotPoses);
+}
+function draw(){
+image(video,0,0,350,300);
+}
+
+
+function take_snapshot(){
+    save('My Filtre Image.png');
+}
+
+function modelloaded()
+{
+console.log("pn is wk")
+}
+
+function gotPoses(results) 
+{
+    if(results.length >0)
+    {
+        console.log(results);
+        console.log("nose x ="+results[0].pose.nose.x);
+        console.log("nose y ="+results[0].pose.nose.y);
+    }
+}
